5f40dc295261ee58d81a082318bff323a4f1a7c5
NakVong <kiriratanakvong@berkeley.edu>
2025-03-08 11:04:15 -0800

    hardcoded version with js running (not perfect behavior)

--- elements/pl-grid/pl-grid.py
+++ elements/pl-grid/pl-grid.py
@@ -10,7 +10,6 @@ def prepare(element_html, data):
 def render(element_html, data):
     html_params = {
         'number': data['params']['random_number'],
-        'image_url': data['options']['client_files_element_url'] + '/block_i.png'
     }
     with open('pl-grid.mustache', 'r') as f:
         return chevron.render(f, html_params).strip()
