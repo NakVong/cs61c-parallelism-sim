4fdbd7346e979b79f1bebf112f4e3b93d2ee62e3
NakVong <kiriratanakvong@berkeley.edu>
2025-03-18 12:04:46 -0700

    gridstack library

--- elements/pl-grid/pl-grid.js
+++ elements/pl-grid/pl-grid.js
@@ -1,3 +1,14 @@
 $(function() {
   $('.example-course-custom-element').append('<p>This text was added by a script.</p>');
+  var items = [
+    {content: 'my first widget'}, // will default to location (0,0) and 1x1
+    {w: 2, content: 'another longer widget!'} // will be placed next at (1,0) and 2x1
+  ];
+
+  var items = [
+    {content: 'my first widget'}, // will default to location (0,0) and 1x1
+    {w: 2, content: 'another longer widget!'} // will be placed next at (1,0) and 2x1
+  ];
+  var grid = GridStack.init();
+  grid.load(items);
 })
