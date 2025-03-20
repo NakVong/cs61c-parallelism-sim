86bf10e10a3210bdd200c3bfeff86f40369d812e
NakVong <kiriratanakvong@berkeley.edu>
2025-03-19 17:53:46 -0700

    freeform (no space savings) and UI changes

--- elements/pl-grid/pl-grid.js
+++ elements/pl-grid/pl-grid.js
@@ -1,28 +1,29 @@
 $(function() {
-  let opts = {
+  let options = {
     dragOut: true, // Allow dragging items out of the grid
-    acceptWidgets: true // Allow dropping items from other grids
+    acceptWidgets: true, // Allow dropping items from other grids
+    float: true // Freeform (without this it tries to minimize free space)
   };
-  let grids = GridStack.initAll(opts); // Initialize all elements with class 'grid-stack'
+  let grids = GridStack.initAll(options); // Initialize all elements with class 'grid-stack'
 
   // Load items into each grid
   grids[0].load([
-    { content: 'init y' },
-    { w:3, h:0.5, content: '#pragma omp parallel' },
-    { w:1.5, h:0.5,content: 'thread 1' },
-    { w:1.5,h:0.5,content: 'thread 2' },
-    { w:1.5, h:0.5,content: 'read y' },
-    { w:1.5,h:0.5,content: 'ready y' },
-    { h:0.5,content: 'y+' },
-    { h:0.5,content: 'y-' },
-    { h:0.5,content: 'y+' },
-    { h:0.5,content: 'y-' },
-    { h:0.5,w:1.5, content: 'load y' },
-    { h:0.5,w:1.5, content: 'load y' },
+    { w:2, h:0.5, content: 'init y'},
+    { w:2, h:0.5, content: '#pragma omp parallel' },
+    { w:2, h:0.5, content: 'thread 1' },
+    { w:2, h:0.5, content: 'thread 2' },
+    { w:2, h:0.5, content: 'read y' },
+    { w:2, h:0.5, content: 'ready y' },
+    { w:2, h:0.5, content: 'y+' },
+    { w:2, h:0.5, content: 'y-' },
+    { w:2, h:0.5, content: 'y+' },
+    { w:2, h:0.5, content: 'y-' },
+    { w:2, h:0.5, content: 'load y' },
+    { w:2, h:0.5, content: 'load y' },
   ]);
 
   grids[1].load([
-    { content: 'main' },
+    { w:2, h:0.5, content: 'main' },
 
   ]);
 });
