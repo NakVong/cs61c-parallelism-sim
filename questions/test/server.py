3666605aa4fc8ed32fa368d34024315b997629f2
Runjeet Narula <runjeetnarula@berkeley.edu>
2025-04-07 10:49:09 -0700

    html data stored in dictioanry for each grid

--- questions/test/server.py
+++ questions/test/server.py
@@ -12,19 +12,21 @@ def parse_html(filepath, tags):
     # Parse the file as HTML
     tree = html.parse(filepath)
     root = tree.getroot()
-
+    
+    html_data = {} 
     # parse each graph
     for tag in tags:
         grid = root.xpath('//pl-grid/' + tag + '/pl-element')
+        grid_data = [] # nested list containing info for each pl-element tag
         for element in grid:
             x = element.get('x')
             y = element.get('y')
             color = element.get('color')
             text = element.text
 
-            lst = [x, y, color, text]
-
-            print(lst)
-                
+            element_data = [x, y, color, text] # list containing individual pl-element attributes
+            grid_data.append(element_data)
+        html_data[tag] = grid_data
+    #print(html_data)        
 def generate(data):
     parse_html(filepath, tags)
