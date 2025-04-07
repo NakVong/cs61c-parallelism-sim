ccde07d6fbc6a17de0bdafc116ac5d6546f93e49
Runjeet Narula <runjeetnarula@berkeley.edu>
2025-04-07 10:40:04 -0700

    parsing html working

--- questions/test/server.py
+++ questions/test/server.py
@@ -2,9 +2,31 @@ import random
 import matplotlib.pyplot as plt
 import prairielearn as pl
 import numpy as np
+from lxml import html
 
-def parse_html(filename):
-    pass
+filepath = "question.html"
+tags = ['pl-source', 'pl-destination', 'pl-answer-grid']
 
+
+def parse_html(filepath, tags):
+    # Parse the file as HTML
+    tree = html.parse(filepath)
+    root = tree.getroot()
+
+    # parse each graph
+    for tag in tags:
+        grid = root.xpath('//pl-grid/' + tag + '/pl-element')
+        for element in grid:
+            #element = root.xpath('//pl-grid/pl-source/pl-element')[i]
+
+            x = element.get('x')
+            y = element.get('y')
+            color = element.get('color')
+            text = element.text
+
+            lst = [x, y, color, text]
+
+            print(lst)
+                
 def generate(data):
-    data["params"]["grid"] = 
+    parse_html(filepath, tags)
