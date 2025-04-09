6c7ec2dbead51167901e8e461646a86b9a9b652d
NakVong <kiriratanakvong@berkeley.edu>
2025-04-09 13:56:32 -0700

    cleaner code

--- elements/pl-grid/pl-grid.py
+++ elements/pl-grid/pl-grid.py
@@ -1,30 +1,33 @@
 import random
+from typing import TypedDict
+
 import chevron
 import lxml.html
 import prairielearn as pl
-
+class GridAnswerData(TypedDict):
+    inner_html: str
+    x: int
+    y: int
 
 def prepare(element_html, data):
     # data['params']['random_number'] = random.random()
     # return data
     element = lxml.html.fragment_fromstring(element_html)
 
-    answer_grid_data = []
+    correct_answers: list[GridAnswerData] = []
 
     for html_tags in element:
         if html_tags.tag == "pl-answer-grid":
-            for el_tag in html_tags:
-                if el_tag.tag == "pl-element":
-                    content = el_tag.text_content().strip()
-                    x = int(el_tag.get("x", 100))
-                    y = int(el_tag.get("y", 100))
-                    answer_grid_data.append({
-                        "content": content,
-                        "x": x,
-                        "y": y
-                        })
-    data["correct_answers"]["grid_answer"] = answer_grid_data
-    print(answer_grid_data)
+            for inner_tag in html_tags:
+                if inner_tag.tag == "pl-element":
+                    answer_data_dict: GridAnswerData = {
+                        "inner_html": inner_tag.text_content().strip(),
+                        "x": int(inner_tag.get("x", 100)),
+                        "y": int(inner_tag.get("y", 100))
+                    }
+                    correct_answers.append(answer_data_dict)
+    data["correct_answers"]["grid_answer"] = correct_answers
+    print(correct_answers)
 
 
 
